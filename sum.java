d6d1fff3e0607f4e2a42a2b9c5f0c226d6270d59
Kapil <kapileswarmoharana32161@gmail.com>
2023-03-27 03:01:50 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,13 @@
+package letslearnGit;
+
+public class sum {
+    
+    public static void main(String[] args)
+    {
+        int a=4;
+        int b=6;
+        System.out.println(a+b);
+
+
+    }
+}
